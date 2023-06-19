1000bbcb2e7d96be595ea869aae2587767282186
Hare Krishna <universeclub73@gmail.com>
2023-06-19 10:26:51 +0530

    local shope

--- lib/View/category_screen/category_screen.dart
+++ lib/View/category_screen/category_screen.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:localshop/consts/consts.dart';
+import 'package:localshop/widgets_common/bg_widget.dart';
 
 class CategoryScreen extends StatelessWidget {
   const CategoryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.blue,);
+    return bgWidget(
+        child: Scaffold(
+      appBar: AppBar(title: categorise.text.fontFamily(bold).white.make()),
+      body: Container(
+        padding: EdgeInsets.all(12),
+        child: GridView.builder(
+            shrinkWrap: true,
+            itemCount: 9,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                mainAxisSpacing: 8,
+                crossAxisSpacing: 8,
+                mainAxisExtent: 200),
+            itemBuilder: (context, index) {
+              return Column(
+
+                children: [
+              Image.asset(icBrands),
+                ],
+              ).box.white.rounded.clip(Clip.antiAlias).outerShadowSm.make();
+            }),
+      ),
+    ));
   }
 }
